5f71bcaf66bb7d5b59cf252d79dae62ab1680367
Johannes Ebeling <johannes@ebelingfs.de>
2016-04-25 08:55:21 +0200

    Started implementing manifest.json and rewriting gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,37 +11,63 @@ var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 
 //Import the manifest file
-//var manifest = require('asset-builder')('./assets/manifest.json');
+var manifest = require('asset-builder')('./assets/manifest.json');
+
+//Initialize all var according to the mainifest.json
+var globs = manifest.globs;
+var project = manifest.getProjectGlobs();
+var paths = manifest.paths;
+
 
 //default task which runs with every start of gulp
 gulp.task('default', ['watch']);
 
 //defines the gulp task for every js file
 gulp.task('js', function(){
-    return gulp.src('source/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-            .pipe(sourcemaps.init())
-                .concat('main.js')
-                //Only uglifies the js files when you run gulp using '--type production', that means only when we are finishing a release
-                .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-            .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/assets/js'));
+    var js = manifest.getDependencyByName("app.js");
+    return gulp.src(js.globs)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(sourcemaps.init())
+    .concat(js.name)
+    //Only uglifies the js files when you run gulp using '--type production', that means only when we are finishing a release
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dist.scripts));
 });
 
 //defines the gulp task for every sass file
 gulp.task('sass', function() {
-  return gulp.src('source/sass/**/*.scss')
+    var sass = getDependencyByName("main.css");
+    return gulp.src(sass.globs)
     .pipe(sourcemaps.init())  // Process the original sources
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(minifyCss())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(minifyCss())
     .pipe(sourcemaps.write()) // Add the map to modified source.
-    .pipe(gulp.dest('public/assets/css'));
+    .pipe(gulp.dest(paths.dist.styles));
 });
+
+gulp.task( 'images', [], function () {
+  return gulp.src( paths.imageSource )
+    .pipe( imageMin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}]
+    }))
+    .pipe( gulp.dest( paths.dist.images ) );
+} );
 
 //defines the watch task
 gulp.task('watch', function(){
-    gulp.watch('source/js/*js', ['js']);
-    gulp.watch('source/sass/*scss', ['sass']);
+    watch(, function(){
+        gulp.start('js');
+    });
+
+    watch('source/sass/*scss', function(){
+        gulp.start('sass');
+    });
+
+    watch( paths.imageSource, function () {
+        gulp.start('images');
+    });
 });
