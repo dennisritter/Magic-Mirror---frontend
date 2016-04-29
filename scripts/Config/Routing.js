d489d60019e3e3aaaab19cd83729d0d49d254db2
nathalie <BHT@Nathalies-MBP.home>
2016-04-29 18:26:47 +0200

    changed initial routing from login.html to start.html

--- scripts/Config/Routing.js
+++ scripts/Config/Routing.js
@@ -5,8 +5,12 @@ angular.module('perna').config(['$routeProvider', '$locationProvider', function 
 
     $routeProvider
         .when('/', {
-            templateUrl: 'layout/login.html',
+            templateUrl: 'layout/start.html',
             activeTab: 'home'
+        })
+        .when('/login', {
+            templateUrl: 'layout/login.html',
+            activeTab: 'login'
         })
         .when('/registration', {
             templateUrl: 'layout/registration.html',
