12e6ef0354eb4d8647e27d1393c4e28e0eb43d8e
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-05 07:02:02 +0200

    Added MouseService;

---
+++ scripts/Service/MouseService.js
@@ -0,0 +1,29 @@
+angular.module('perna').service('MouseService', ['$timeout', function ($timeout) {
+
+    this.mouseData = {
+        isMoving: false
+    };
+
+    var mouseIsMoving = function () {
+        this.mouseData.isMoving = true;
+        stopTimer();
+        startTimer();
+    }
+    var stopTimer = function(){
+        $timeout.cancel(isMovingTimer);
+    }
+
+    var _service = this;
+    var isMovingTimer;
+    var startTimer = function () {
+        isMovingTimer = $timeout(function () {
+            _service.mouseData.isMoving = false;
+        }, 2000);
+    }
+
+    return {
+        mouseData: this.mouseData,
+        mouseIsMoving: mouseIsMoving
+    };
+}]);
+
