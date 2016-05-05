2516d7c0a7cd9466639d95812e76a51f3b725f83
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-05 15:08:06 +0200

    Added  template, controller, route for usersettings

---
+++ scripts/Controller/settingsCtrl.js
@@ -0,0 +1,6 @@
+angular.module('perna').controller('settingsCtrl', ['$scope',
+    function ($scope) {
+        $scope.googleSignIn = function(){
+            console.log('GoogleSignIn is not implemented yet');
+        };
+    }]);
