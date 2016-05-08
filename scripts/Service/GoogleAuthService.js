c63ef58d996a2f5560fffc6b7ac7fdc76a54c603
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-08 21:38:16 +0200

    Bedingungen für reject verkürzt

--- scripts/Service/GoogleAuthService.js
+++ scripts/Service/GoogleAuthService.js
@@ -2,7 +2,8 @@ angular.module('perna').service('GoogleAuthService', ['$http', '$q',
     function ($http, $q) {
 
         /**
-         * Requests a Google Auth URL from the server.
+         * Requests a GoogleAuth URL and a stateToken from the Server.
+         * On success: Creates an eventlistener which listens for a message that represents the finished Google Authentification.
          * @param accessToken
          * @returns {Promise}
          */
@@ -12,30 +13,25 @@ angular.module('perna').service('GoogleAuthService', ['$http', '$q',
                 url: "http://api.perna.dev/v1/google-auth/auth-url",
                 method: "GET",
                 headers: {
-                    'Access-Token' : accessToken
+                    'Access-Token': accessToken
                 }
             })
-                .success(function(response){
+                .success(function (response) {
                     //response.data.url : the GoogleOAuth URL received from the server
                     var popupGoogleAuth = $window.open(response.data.url, "googleAuth", "width=500,height=400");
                     //response.data.state : a unique Session ID to identify the users AuthSession
                     var state = response.data.state;
-                    $window.addEventListener("pernaGoogleAuth", function(event){
-                        if(event.origin !== "http://api.perna.dev"){
-                            defer.reject(event.data);
-                        }
-                        if(!event.data.success){
-                            defer.reject(event.data);
-                        }
-                        if(state !== event.data.state){
+                    $window.addEventListener("pernaGoogleAuth", function (event) {
+                        if (event.origin !== "http://api.perna.dev" || !event.data.success || state !== event.data.state) {
                             defer.reject(event.data);
                         }
                         popupGoogleAuth.close();
                         defer.resolve();
+                        //$window.removeEventListener("pernaGoogleAuth");
                     }, false);
                     //Eventlistener am Ende wieder abmelden
                 })
-                .error(function(response){
+                .error(function (response) {
                     defer.reject(response);
                 });
             return defer.promise;
