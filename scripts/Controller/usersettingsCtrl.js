894921ef70c20d9414339053fa6a746265d8e070
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-08 13:02:18 +0200

    implemented GoogleOAuthService to get an URL from the server

--- scripts/Controller/usersettingsCtrl.js
+++ scripts/Controller/usersettingsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('perna').controller('usersettingsCtrl', ['$scope', 'GoogleOAuthService',
     function ($scope, GoogleOAuthService) {
         $scope.googleOAuth = function(){
-
+            
         };
     }]);
--- scripts/Service/GoogleOAuthService.js
+++ scripts/Service/GoogleOAuthService.js
@@ -1,4 +1,24 @@
-angular.module('perna').service('GoogleOAuthService', ['$http', '$q', function ($http, $q) {
+angular.module('perna').service('GoogleOAuthService', ['$http', '$q', 'CookieService',
+    function ($http, $q, CookieService) {
+        var getGoogleOAuthURL = function () {
+            var accessToken = CookieService.getCookies().accessToken;
+            $http({
+                http: "http://api.perna.dev/v1/google-auth/auth-url",
+                method: "GET",
+                data: accessToken
+            })
+                .success(function(response){
+                    defer.resolve(response);
+                })
+                .error(function(response){
+                    defer.reject(response);
+                });
+            return defer.promise;
 
-    
-}]);
+        };
+
+        return {
+            getGoogleOAuthURL: getGoogleOAuthURL
+        }
+
+    }]);
