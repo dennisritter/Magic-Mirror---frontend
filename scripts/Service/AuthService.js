3982a1de77f165c33070f5b7ad2e8c7afabe745c
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-11 18:51:53 +0200

    added autoLogin method

--- scripts/Service/AuthService.js
+++ scripts/Service/AuthService.js
@@ -4,11 +4,33 @@ angular.module('perna').service('AuthService', ['$http', '$q', 'CookieService',
         var AuthService = function () {
             this.credentials = {
                 accessToken: undefined,
-                refreshToken: undefined
+                refreshToken: {
+                    accessToken: undefined,
+                    refreshToken: undefined
+                }
+
             };
         };
 
-        
+        AuthService.prototype.autoLogin = function (data) {
+            var _authService = this;
+            var defer = $q.defer();
+            $http({
+                url: "http://api.perna.dev/v1/refresh",
+                method: "POST",
+                data: data
+            })
+                .success(function (response) {
+                    _authService.credentials.accessToken = response.data.token;
+                    _authService.credentials.refreshToken = response.data.refreshToken.token;
+                    CookieService.setCookies(response);
+                    defer.resolve();
+                })
+                .error(function (response) {
+                    defer.reject(response);
+                });
+            return defer.promise;
+        };
 
         AuthService.prototype.login = function (data) {
             var _authService = this;
