29e8377e58c9e7e2dcfe7f5564e0f941f69f421d
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-13 16:42:53 +0200

    Bugfix for an ui-router error

--- scripts/Config/Routing.js
+++ scripts/Config/Routing.js
@@ -3,8 +3,13 @@
  * @name Routing
  * @desc Defines which template should be loaded for a specific url affix
  */
-angular.module('perna').config(['$stateProvider', '$locationProvider', '$urlRouterProvider',
-    function ($stateProvider, $locationProvider, $urlRouterProvider) {
+angular.module('perna').config(['$qProvider', '$stateProvider', '$locationProvider', '$urlRouterProvider',
+    function ($qProvider, $stateProvider, $locationProvider, $urlRouterProvider) {
+
+        // fixes a ui-router Bug with ui.router module, showing unhandled rejection errors
+        // source: https://github.com/angular-ui/ui-router/issues/2699
+        // --> https://docs.angularjs.org/api/ng/provider/$qProvider
+        $qProvider.errorOnUnhandledRejections(false);
 
         var authRedirect = ['AuthService', '$state', function (AuthService, $state) {
             if (!AuthService.isAuthenticated) {
