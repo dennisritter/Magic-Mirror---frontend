8f0400d7cf030f650a7dcf7d96b11d418ab2ad16
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-05-17 11:17:16 +0200

    Added CalendarService providing methods to request calendarData from Server

---
+++ scripts/Service/CalendarService.js
@@ -0,0 +1,62 @@
+angular.module('perna').service('calendarService', ['$http', '$q', 'AuthService',
+    function ($http, $q, AuthService) {
+
+        /**
+         * A list of the users calendars.
+         */
+        var calendarService = function(){
+            this.calendars = [];
+        };
+
+        /**
+         * Requests the users calendars from Server.
+         * @param accessToken
+         * @returns {Promise}
+         */
+        calendarService.prototype.getCalendars = function() {
+            var _calendarService = this;
+            var defer = $q.defer();
+            $http({
+                url: "http://api.perna.dev/v1/calendar/calendars",
+                method: "GET",
+                headers :{
+                    'Access-Token': AuthService.credentials.accessToken
+                }
+            })
+                .success(function(response){
+                    _calendarService.calendars = response.data;
+                    defer.resolve(response);
+                })
+                .error(function(response){
+                    defer.reject(response);
+                });
+            return defer.promise;
+        };
+
+        /**
+         * Requests the Events of the given calendars from the Server
+         * @param calendars     an array of calendarIds
+         */
+        calendarService.prototype.getEvents = function(calendarIds){
+            var defer = $q.defer();
+            console.log(calendarIds.join(","));
+            $http({
+                url: "http://api.perna.dev/v1/calendar/events",
+                method: "GET",
+                headers :{
+                    'Access-Token': AuthService.credentials.accessToken
+                },
+                params: {calendarIds: calendarIds.join(",")}
+            })
+                .success(function(response){
+                    defer.resolve(response);
+                })
+                .error(function(response){
+                    defer.reject(response);
+                });
+            return defer.promise;
+        };
+
+        return new calendarService();
+
+    }]);
