a29b902ae1c5704a5909bb234fb9e4013743f9e7
Johannes Ebeling <johannes@ebelingfs.de>
2016-05-18 13:39:01 +0200

    Added constants to app.js

--- scripts/App.js
+++ scripts/App.js
@@ -3,4 +3,15 @@ angular.module('perna', [
     'ui.router',
     'ngPassword',
     'ngCookies',
-]);
+]).constant('routes', {
+    home : 'layout/start.html',
+    dashboard : 'layout/dashboard.html',
+    usersettings : 'layout/usersettings.html'
+}).constant('api', {
+    source : 'http://api.perna.dev',
+    login : 'http://api.perna.dev/v1/login',
+    save : 'http://api.perna.dev/v1/register',
+    refresh : 'http://api.perna.dev/v1/refresh',
+    logout : 'http://api.perna.dev/v1/logout',
+    gauth : 'http://api.perna.dev/v1/google-auth/auth-url'
+});
