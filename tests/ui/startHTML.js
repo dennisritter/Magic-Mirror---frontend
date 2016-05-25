6a86ed5ed9f36c22411863caadfa099265ac647b
Johannes Ebeling <johannes@ebelingfs.de>
2016-05-25 15:53:22 +0200

    first structural test

--- tests/ui/startHTML.js
+++ tests/ui/startHTML.js
@@ -1,11 +1,16 @@
-var casper = require('casper').create();
+casper.test.begin('Test structure of start.html', 5, function suite(test){
+    casper.start("http://perna.dev/", function() {
+        test.assertTitle('perna. smart - mirror - cloud', 'The title exists, page has sucessfully loaded');
+        test.assertExists('#panel-element-login', 'The login element exists');
+        test.assertExists('#loginForm', 'The login form has loaded');
+        test.assertExists('#panel-element-registration', 'The registration element exists');
+    });
 
-casper.test
-
-casper.test.begin('The heading exists', 1, function suite(test) {
-    casper.start('http://domain.tld/page.html', function() {
-        test.assertExists('h1.page-title');
-    }).run(function() {
+    casper.then(function(){
+        this.click('#panel-element-registration')
+        test.assertExists('#registrationForm', 'After clicking, the registration form is now visible');
+    });
+    casper.run(function() {
         test.done();
     });
 });
