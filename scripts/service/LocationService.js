2b4283cc0ad6a75d860a3a4afce58a0ff864cbaa
Johannes Ebeling <johannes@ebelingfs.de>
2016-05-29 22:22:52 +0200

    Haha Nevermind

--- scripts/service/LocationService.js
+++ scripts/service/LocationService.js
@@ -5,43 +5,13 @@
  * @return An Instance of LocationService
  */
 
- angular.module('perna').service('LocationService', ['$http', '$q',
-     function ($http, $q) {
+ angular.module('perna').service('LocationService', ['$http', '$q', 'api',
+     function ($http, $q, api) {
 
         var LocationService = function () {
-            var userCoords = {
-                lat : undefined,
-                long : undefined
-            },
 
-            var userLocationID = undefined;
+            this.userLocationID = undefined;
          };
-
-        LocationService.prototype.getLocationIDWithUserInput(input){
-            var _authService = this;
-            var defer = $q.defer();
-            $http({
-                url: api.refresh,
-                method: "POST",
-                data: data
-            })
-                .success(function (response) {
-                    _authService.credentials.accessToken = response.data.token;
-                    _authService.credentials.refreshToken = response.data.refreshToken.token;
-                    CookieService.setCookies(response);
-                    $http.defaults.headers.common = {'Access-Token' : _authService.credentials.accessToken};
-                    _authService.isAuthenticated = true;
-                    defer.resolve();
-                })
-                .error(function (response) {
-                    defer.reject(response);
-                });
-            return defer.promise;
-        };
-
-        LocationService.prototype.getLocationIDWithAutocomplete(){
-
-        }
 
          return new LocationService();
      }]);
