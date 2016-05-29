b41eb57a7f26e385cfda02a0aca435c6ca90dc7f
Johannes Ebeling <johannes@ebelingfs.de>
2016-05-29 21:07:39 +0200

    Started LocationService

--- scripts/service/LocationService.js
+++ scripts/service/LocationService.js
@@ -1,6 +1,19 @@
 /**
  * @author Johannes Ebeling
- * @name AuthService
- * @desc A Service which includes functions for user autentification
- * @return An Instance of AuthService
+ * @name LocationService
+ * @desc A Service which includes functions for getting the users location
+ * @return An Instance of LocationService
  */
+
+ angular.module('perna').service('AuthService', ['$http', '$q',
+     function ($http, $q) {
+
+        var LocationService = function () {
+         };
+
+        LocationService.prototype.getLocationID(input){
+            
+        }
+
+         return new LocationService();
+     }]);
