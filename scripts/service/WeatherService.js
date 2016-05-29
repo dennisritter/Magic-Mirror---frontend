1ab5bcee20425481fe6d0e0e966bba102ab44248
nathalie <BHT@Nathalies-MacBook-Pro.local>
2016-05-29 16:47:43 +0200

    service

--- scripts/service/WeatherService.js
+++ scripts/service/WeatherService.js
@@ -1,3 +1,34 @@
 /**
- * Created by BHT on 27.05.16.
+ * Created by nathalie on 27.05.16.
  */
+
+angular.module('perna').service('WeatherService', ['$http', '$q', 'api',
+    function ($http, $q, api) {
+    
+    var WeatherService = function (){
+        this.autocomplCity;
+    }
+        
+    WeatherService.prototype.autocomplete = function ( query, accesstoken ) {
+        var _weatherService = this;
+        var deferred = $q.defer();
+        $http({
+            url: api.autocomplete(query, accesstoken),
+            method: "GET"
+        })
+            .success(function(response){
+                _weatherService.autocomplCity = response.data;
+                deferred.resolve(response);
+            })
+            .error(function(response){
+                deferred.reject(response);
+            })
+
+        return deferred.promise;
+    };
+    
+    
+    
+    
+    
+    }]);
