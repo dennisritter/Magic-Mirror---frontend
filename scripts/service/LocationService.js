d82ba73e18db3ae6672a11745ddeb24c9bdc945e
Johannes Ebeling <johannes@ebelingfs.de>
2016-05-30 12:23:31 +0200

    Progress

--- scripts/service/LocationService.js
+++ scripts/service/LocationService.js
@@ -12,5 +12,16 @@
             this.userLocationID = undefined;
          };
 
+         LocationService.prototype.determineUserLocation = function() {
+            //get LocationID using the html5 geolocation api and perna api
+            var deferred = $q.defer();
+            $http({
+                url : api.weather_nearby;
+            })
+         }
+
+         LocationService.prototype.provideAutocompleteResults = function(query) {
+            //get up to 10 LocationID`s for the provided search query and provide them to the calling function
+         }
          return new LocationService();
      }]);
