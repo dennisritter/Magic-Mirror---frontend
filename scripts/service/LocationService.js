e734680ca532f38713185895fe076692c52d3cbd
Johannes Ebeling <johannes@ebelingfs.de>
2016-05-31 16:17:40 +0200

    Implemented both methods, fixed "GET" requests

--- scripts/service/LocationService.js
+++ scripts/service/LocationService.js
@@ -1,47 +1,52 @@
 /**
- * @author Johannes Ebeling
- * @name LocationService
- * @desc A Service which includes functions for getting the users location
- * @return An Instance of LocationService
- */
+* @author Johannes Ebeling
+* @name LocationService
+* @desc A Service which includes functions for getting the users location
+* @return An Instance of LocationService
+*/
 
- angular.module('perna').service('LocationService', ['$http', '$q', 'api',
-     function ($http, $q, api) {
+angular.module('perna').service('LocationService', ['$http', '$q', 'api',
+function ($http, $q, api) {
 
-        var LocationService = function () {
-            this.userLocationID = undefined;
-         };
+    var LocationService = function () {
+        this.userLocationID = undefined;
+    };
 
-         LocationService.prototype.determineUserLocation = function() {
-            var locationCoords = ;
-            var deferred = $q.defer();
-            $http({
-                url : api.weather_nearby,
-                method : 'GET',
-                data : locationCoords
-            })
-            .success(function(response){
-                this.userLocationID = response.id;
-                defer.resolve();
-            })
-            .error(function(response){
-                defer.reject(response);
-            })
-         }
+    LocationService.prototype.determineUserLocation = function() {
+        var locationCoords = navigator.geolocation.getCurrentLocation();
+        var deferred = $q.defer();
+        $http({
+            url : api.weather_nearby,
+            method : 'GET',
+            params : {
+                latitude : locationCoords.latitude,
+                longitude : locationCoords.longitude
+            }
+        })
+        .success(function(response){
+            this.userLocationID = response.id;
+            defer.resolve();
+        })
+        .error(function(response){
+            defer.reject(response);
+        })
+    };
 
-         LocationService.prototype.provideAutocompleteResults = function(query) {
-            var deferred = $q.defer();
-            $http({
-                url : api.weather_autocomplete,
-                method : 'GET',
-                data : query
-            })
-            .success(function(response){
-                defer.resolve(response.data);
-            })
-            .error(function(response){
-                defer.reject(response);
-            })
-         }
-         return new LocationService();
-     }]);
+    LocationService.prototype.provideAutocompleteResults = function(query) {
+        var deferred = $q.defer();
+        $http({
+            url : api.weather_autocomplete,
+            method : 'GET',
+            params : {
+                query : query
+            }
+        })
+        .success(function(response){
+            defer.resolve(response.data);
+        })
+        .error(function(response){
+            defer.reject(response);
+        })
+    };
+    return new LocationService();
+}]);
