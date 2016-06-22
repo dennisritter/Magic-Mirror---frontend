1b1ef74098acaec634aaa6dd34515bf749e90794
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-06-22 15:31:22 +0200

    cleaned PTService

--- scripts/directive/modulePublicTransport.js
+++ scripts/directive/modulePublicTransport.js
@@ -15,26 +15,8 @@ angular.module('perna').directive('modulePublicTransport', ['routes',
             },
             controller: ['$scope', 'PublicTransportLocationService', 'LiveviewService',
                 function( $scope, PublicTransportLocationService, LiveviewService ){
-                    
+
                     $scope.configMode = true;
-
-                    /**
-                     * Searches for a location which fits the query and stores the found location in the
-                     * locationFound-variable.
-                     * @param query  The query.
-                     */
-                    $scope.searchLocation = function (query){
-                        var successCallback = function (response){
-                            $scope.locationFound = response;
-                            $scope.locationsDetected = true;
-                        };
-                        var errorCallback = function (response){
-                            console.error(response);
-                        };
-
-                        PublicTransportLocationService.searchGeonames(query).then(successCallback, errorCallback);
-
-                    };
 
                     var persist = function(){
                         LiveviewService.persist();
@@ -60,14 +42,7 @@ angular.module('perna').directive('modulePublicTransport', ['routes',
                         };
                         LiveviewService.deleteModule($scope.module).then(successCallback, errorCallback);
                     };
-                }],
-            link: function(scope){
-                if(scope.module.locationId !== 0){
-                    scope.getLocationName(scope.module.locationId);
-                    scope.configMode = false;
-                }else {
-                    scope.configMode = true;
-                }
-            }
+                }]
+            //link:
         };
     }]);
