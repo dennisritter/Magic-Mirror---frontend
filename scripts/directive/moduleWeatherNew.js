990211558e5927b04e24216e4eedc3d0689944bf
Jannik Portz <hello@jannikportz.de>
2016-07-06 22:35:04 +0200

    added comment

--- scripts/directive/moduleWeatherNew.js
+++ scripts/directive/moduleWeatherNew.js
@@ -73,6 +73,21 @@ angular.module('perna').directive('moduleWeather', ['routes', function (routes) 
           }];
 
         $scope.edit = function () {
+          /**
+           * Modal I/O
+           *
+           * You may provide a key/value hash as 'inputs' in the options,
+           * inputs will be injected into your controller if you require them.
+           * e.g.:
+           * inputs: {
+           *  moduleData: $scope.module
+           * }
+           * can be accessed in your controller's dependencies as 'moduleData'.
+           * As output you may use the first parameter of the 'close' function.
+           *
+           * showModal returns a promise resolving with the data passed to the 'close' function.
+           * the promise rejects with no data when passing -1 to 'close'
+           */
           PernaModalService.showModal({
             controller: editController,
             templateUrl: 'directive/modules/module-weather-edit.html'
